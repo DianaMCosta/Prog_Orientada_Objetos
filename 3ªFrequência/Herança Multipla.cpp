b9cb2e92a29207c21171c1e73ef3c4175b232feb
Diana Costa <129592229+dianamanuela@users.noreply.github.com>
2024-01-17 19:57:50 +0000

    Update Herança Multipla.cpp

--- 3ªFrequência/Herança Multipla.cpp
+++ 3ªFrequência/Herança Multipla.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
 
+using namespace std;
+
+class Base1{
+public: 
+    void frase1(){
+        cout << "classe base 1" << endl;
+    }
+    void f(){
+        cout << "OLÁ\n";
+    }
+};
+
+class Base2{
+public:
+    void frase2(){
+        cout << "classe base 2"<<endl;
+    }
+    
+    void f(){
+        cout << "Holanda\n";
+    }
+    
+};
+
+class Derivada: public Base2 , public Base1{
+public:
+
+    void frase3(){
+            cout << "classe base 3" <<endl;
+        }
+};
+
+
+
+int main(){
+
+    Derivada objeto;
+
+    objeto.Base1::f();
+    objeto.Base2::f();
+    objeto.frase1();
+
+    return 0;
+}
